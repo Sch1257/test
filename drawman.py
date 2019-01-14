7fb8e2363b3b948e004cdf9cd7fd76aee9d98c40
dmv77 <dmv77@mail.ru>
2019-01-14 23:52:21 +0300

    Пробная версия Чертежника

---
+++ drawman.py
@@ -0,0 +1,29 @@
+from turtle import Turtle
+t = Turtle()
+t.penup()
+
+def test_drawman():
+    pen_down()
+    for i in range(5)
+        on_vector(1, 2)
+        on_vector(0, -2)
+        pen_up()
+        to_point(0, 0)
+def pen_up():
+    t.penup()
+
+
+def pen_down():
+    t.pendown()
+
+
+def on_vector(x, y):
+    pass
+
+
+def to_point(x, y):
+    pass
+
+
+if __name__ == (__main__):
+    test_drawman()
